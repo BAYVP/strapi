2125874356639443a563137fbef087db6be2194a
manishsg <manishsg@hotmail.com>
2018-06-10 18:55:21 -0700

    first checkin

---
+++ events/controllers/Events.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * Events.js controller
+ *
+ * @description: A set of functions called "actions" for managing `Events`.
+ */
+
+module.exports = {
+
+  /**
+   * Retrieve events records.
+   *
+   * @return {Object|Array}
+   */
+
+  find: async (ctx) => {
+    return strapi.services.events.fetchAll(ctx.query);
+  },
+
+  /**
+   * Retrieve a events record.
+   *
+   * @return {Object}
+   */
+
+  findOne: async (ctx) => {
+    if (!ctx.params._id.match(/^[0-9a-fA-F]{24}$/)) {
+      return ctx.notFound();
+    }
+
+    return strapi.services.events.fetch(ctx.params);
+  },
+
+  /**
+   * Create a/an events record.
+   *
+   * @return {Object}
+   */
+
+  create: async (ctx) => {
+    return strapi.services.events.add(ctx.request.body);
+  },
+
+  /**
+   * Update a/an events record.
+   *
+   * @return {Object}
+   */
+
+  update: async (ctx, next) => {
+    return strapi.services.events.edit(ctx.params, ctx.request.body) ;
+  },
+
+  /**
+   * Destroy a/an events record.
+   *
+   * @return {Object}
+   */
+
+  destroy: async (ctx, next) => {
+    return strapi.services.events.remove(ctx.params);
+  }
+};
